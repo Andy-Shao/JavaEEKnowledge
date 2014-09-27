555ce197c4c5dddfc9718d09fba7b7b2f26cf2ce
root <root@andyshao-System-Product-Name>
2014-09-28 01:03:37 +0800

    add faces_detail
    Signed-off-by: root <root@andyshao-System-Product-Name>

---
+++ src/main/java/oralce/offical/javaee/faces/MyRandomNumber.java
@@ -0,0 +1,27 @@
+package oralce.offical.javaee.faces;
+
+import java.util.Random;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class MyRandomNumber {
+
+    private final Random random = new Random();
+    
+    public boolean getRandomBoolean(){
+        return this.random.nextBoolean();
+    }
+    
+    public boolean giveTrue(){
+        return true;
+    }
+    
+    public boolean giveFalse(){
+        return false;
+    }
+    
+    public String[] items(){
+        return new String[]{"Maven Book", "SubVersion Book", "Other Book"};
+    }
+}
