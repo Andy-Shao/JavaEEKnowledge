f17379c6b13256a7f59039c5e53460ce5236bda8
Andy-Shao <hj13035952619@126.com>
2014-09-30 14:50:37 +0800

    clean Client
    Signed-off-by: Andy-Shao <hj13035952619@126.com>

--- src/main/java/oralce/offical/javaee/web/services/HelloAppClient.java
+++
@@ -1,7 +0,0 @@
-package oralce.offical.javaee.web.services;
-
-public class HelloAppClient {
-//	@WebServiceRef(wsdlLocation = "META-INF/wsdl/localhost_8080/helloservice/HelloService.wsdl")
-//	private static HelloService service;
-
-}
