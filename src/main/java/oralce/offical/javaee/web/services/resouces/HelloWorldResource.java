50f0d1f8eea3cd93d623132e85e9b052114e5947
Andy-Shao <hj13035952619@126.com>
2014-09-30 17:02:56 +0800

    rest WS

---
+++ src/main/java/oralce/offical/javaee/web/services/resouces/HelloWorldResource.java
@@ -0,0 +1,18 @@
+package oralce.offical.javaee.web.services.resouces;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+//The Java class will be hosted at the URI path "/helloworld"
+@Path("/rest/helloworld")
+public class HelloWorldResource {
+
+	//The Java method will process HTTP GET requests
+	@GET
+	//The Java method will produce context identified by the MIME Media type "text/plain"
+	@Produces("text/plain")
+	public String getClichedmessage() {
+		return "Hello World";
+	}
+}
