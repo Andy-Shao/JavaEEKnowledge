f454f4ea01508ebf7a5425c999ea52b6d2fc22f1
Andy-Shao <hj13035952619@126.com>
2014-11-04 23:58:47 +0800

    first demo

---
+++ src/main/java/hello1/Hello.java
@@ -0,0 +1,18 @@
+package hello1;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class Hello {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
