ec6695853f4cd87f17990d31664b3ddd45ffd1b6
Andy-Shao <hj13035952619@126.com>
2015-09-13 17:27:50 +0800

    add skier

--- ch3/skier/src/main/java/Person.java
+++ ch3/skier/src/main/java/Person.java
@@ -1,29 +1,45 @@
 import javax.xml.bind.annotation.XmlType;
 
-@XmlType       
-public class Person  {
-            
+@XmlType
+public class Person {
+
+    private int age;
+    private String gender;
     // fields
     private String name;
-    private int    age;
-    private String gender;
-        
+
     // constructors
-    public Person() { }
-    
-    public Person(String name, int age, String gender){
-	setName(name);
-	setAge(age);
-	setGender(gender);
+    public Person() {
+    }
+
+    public Person(String name , int age , String gender) {
+        this.setName(name);
+        this.setAge(age);
+        this.setGender(gender);
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getGender() {
+        return this.gender;
     }
 
     // properties: name, age, gender
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    
-    public int getAge() { return age;  }
-    public void setAge(int age) { this.age = age; }
-    
-    public String getGender() { return gender; }
-    public void setGender(String gender) { this.gender = gender; }
+    public String getName() {
+        return this.name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- ch3/skier/src/main/java/Skier.java
+++ ch3/skier/src/main/java/Skier.java
@@ -1,37 +1,47 @@
+import java.util.Collection;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
-import java.util.Collection;
-
 @XmlRootElement
-public class Skier  {    
-    private Person person;    
-    private String nationalTeam; 
+public class Skier {
     @SuppressWarnings("rawtypes")
     private Collection majorAchievements;
-    
+    private String nationalTeam;
+    private Person person;
+
     // constructors
-    public Skier() { } // required for unmarshaling
-    public Skier (Person person, 
-		  String nationalTeam, 
-		  Collection<String> majorAchievements) {
-        setPerson(person);
-	setNationalTeam(nationalTeam);
-	setMajorAchievements(majorAchievements);
+    public Skier() {
+    } // required for unmarshaling
+
+    public Skier(Person person , String nationalTeam , Collection<String> majorAchievements) {
+        this.setPerson(person);
+        this.setNationalTeam(nationalTeam);
+        this.setMajorAchievements(majorAchievements);
     }
-    
-    // properties
-    public Person getPerson() { return this.person; }
-    public void setPerson (Person person) { this.person = person; }
-    
-    public String getNationalTeam() { return this.nationalTeam; }
-    public void setNationalTeam(String nationalTeam) {        
-        this.nationalTeam = nationalTeam;
-    }    
-           
+
     @SuppressWarnings("rawtypes")
-    public Collection getMajorAchievements() { return majorAchievements; }
+    public Collection getMajorAchievements() {
+        return this.majorAchievements;
+    }
+
+    public String getNationalTeam() {
+        return this.nationalTeam;
+    }
+
+    // properties
+    public Person getPerson() {
+        return this.person;
+    }
+
     public void setMajorAchievements(@SuppressWarnings("rawtypes") Collection majorAchievements) {
         this.majorAchievements = majorAchievements;
+    }
+
+    public void setNationalTeam(String nationalTeam) {
+        this.nationalTeam = nationalTeam;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
     }
 }
