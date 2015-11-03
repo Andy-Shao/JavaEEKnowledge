2be4e8d37bfd80726842d32c58b21078c19deca0
Andy-Shao <hj13035952619@126.com>
2015-11-04 00:40:03 +0800

    remove

--- ch5/predictionsSOAP/src/main/java/predictions/VerbosityException.java
+++ ch5/predictionsSOAP/src/main/java/predictions/VerbosityException.java
@@ -1,7 +1,7 @@
 package predictions;
 
+@SuppressWarnings("serial")
 public class VerbosityException extends Exception {
-    private static final long serialVersionUID = -4786508294943139666L;
     private String details;
 
     public VerbosityException(String reason , String details) {
